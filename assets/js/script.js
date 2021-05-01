251c546e6e2a70ee5c3914315699ed93c9283fb7
Mona Lisa <sam.roberson32@gmail.com>
2021-05-01 02:23:23 -0600

    updated html with css, bootstrap styling before working on js

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// figure out the weather api and how to link it
+
+// need a form and a button maybe statically---done 
+
+
+
+//need the button to talk to the weather api with the users input
+
+
+
+// need the input of the user to go to the api and store the data in the browser
+
+
+
+// need to display the data from storage that was retrieved by the api on the screen in a weather widget
+
+
+
+//need the weather of the searched area to display a 5 day forecast under the today forecast maybe with cards, not statically? or statically?
+
+
+
+//need all of the previous searches to display under the search bar as buttons to be pressed
